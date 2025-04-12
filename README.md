afee7637f12dc0924aaa0525ae30924c4bfe8c86
Karen Wei <karenweidev@gmail.com>
2025-04-12 12:04:22 -0400

    Add model scene

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This app is created for drawing practice with 3D spatial object
+
+### Installation
+```bash
+npm install
+```
+
+### Run the app
+```bash
+npm run dev
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState, useRef } from 'react'
+import * as THREE from 'three';
 import './App.css'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import Scene from './Scene';
+
+// function MyThree() {
+//   // Instantiate glTF Loader
+//   const loader = new GLTFLoader();
+
+//   useEffect(() => {
+//     // === THREE.JS CODE START ===
+//     var scene = new THREE.Scene();
+//     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//     var renderer = new THREE.WebGLRenderer();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     document.body.appendChild( renderer.domElement );
+
+//     // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//     // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//     // const cube = new THREE.Mesh( geometry, material );
+//     // scene.add( cube );
+//     // camera.position.z = 5;
+
+//     // var animate = function () {
+//     //   requestAnimationFrame(animate);
+//     //   cube.rotation.x += 0.01;
+//     //   cube.rotation.y += 0.01;
+//     //   renderer.render(scene, camera);
+//     // };
+//     // animate();
+
+//     // Load a glTF resource
+//     const dracoLoader = new DRACOLoader();
+//     dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
+
+//     loader.setDRACOLoader( dracoLoader );
+//     loader.load(
+//       // resource URL
+//       'mug.glb', 
+//       // called when the resource is loaded
+//       function ( gltf ) {
+//         scene.add( gltf.scene );
+//       }, 
+//       // called while loading is progressing
+//       function ( xhr ) {
+//         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+//       },
+//       // called when loading has errors
+//       undefined, function ( error ) {
+//         console.error( error );
+//     } );
+//   }, []);
+
+// }
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const earth = useGLTF('./mug.glb');
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Scene />
     </>
   )
 }
